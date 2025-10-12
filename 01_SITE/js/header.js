c0a21a227f720b7a35afa3beab8585bab512974b
livrariaprometeu <livrariaprometeu@gmail.com>
2025-10-12 10:11:14 -0300

    Update header.js

--- 01_SITE/js/header.js
+++ 01_SITE/js/header.js
@@ -2,7 +2,7 @@ function loadHeader() {
   const headerHTML = `
     <header class="container" role="banner" aria-label="Cabeçalho do site">
       <a href="https://livrariaprometeu.com/index.html" class="brand" aria-hidden="false">
-        <img src="https://livrariaprometeu.com/03_MIDIA/logos/livraria_prometeu_original.webp" alt="Logotipo Livraria Prometeu">
+        <img src="https://livrariaprometeu.com/03_MIDIA/logos/Untitled%20design.png" alt="Logotipo Livraria Prometeu">
       <a>  
       <nav class="nav-wrap" role="navigation" arial-label="Menu principal">
         <a href="https://livrariaprometeu.com/01_SITE/html/artigos/index.html">Blog</a>
