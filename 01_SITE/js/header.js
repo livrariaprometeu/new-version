c5563f87b993ac71c5f1ea9ef34320453ad85f65
livrariaprometeu <livrariaprometeu@gmail.com>
2025-11-01 13:34:23 -0300

    Update header.js

--- 01_SITE/js/header.js
+++ 01_SITE/js/header.js
@@ -12,7 +12,6 @@ function loadHeader() {
       <nav class="nav-wrap" role="navigation" aria-label="Menu principal">
         
         <a href="https://livrariaprometeu.com/blog">Blog</a>
-        <a href="https://livrariaprometeu.com">Home</a>
         <a href="https://livrariaprometeu.com/livros">Books</a>
 
         ${
